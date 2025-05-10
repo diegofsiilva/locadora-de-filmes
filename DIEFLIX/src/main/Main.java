2fb8750b67a75504273a020ff63cd33b1b98f4b9
Diego Figueiredo <diego.silva@sou.inteli.edu.br>
2025-05-10 16:07:17 -0300

    feat/att final

---
+++ DIEFLIX/src/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import View.tela1_GUI;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     new tela1_GUI().setVisible(true);
+    }
+    
+}
---
+++ action/teste.py
@@ -0,0 +1,5 @@
+n1=int(input("Digite um número: "))
+n2=int(input("Digite um outro número:"))
+
+mult=(n1*n2)
+print("O resultado dessa multiplicação é ",mult)
